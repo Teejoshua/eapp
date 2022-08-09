f4f04cb364643f4aab4e07e92bef2787e7ea9286
teejoshua <teejoshua01@gmail.com>
2022-08-09 17:54:31 +0100

    Initial commit

---
+++ eapp.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using eapp.ServiceModel;
+
+namespace eapp.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
